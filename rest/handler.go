4460abf502e2cfb4a7a31226c6b912ccf013500b
Taeho0927 <rlfxogh0927@gmail.com>
2023-01-08 16:12:38 +0900

    23.01.08 update

--- rest/handler.go
+++ rest/handler.go
@@ -2,7 +2,9 @@ package rest
 
 import (
 	"GoMusicAPI/dblayer"
+	"GoMusicAPI/models"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -34,4 +36,82 @@ func (h *Handler) GetProducts(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, products)
+}
+func (h *Handler) GetPromos(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	promos, err := h.db.GetPromos()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, promos)
+}
+func (h *Handler) SignIn(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	var customer models.Customer
+	err := c.ShouldBindJSON(&customer)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	customer, err = h.db.SignInUser(customer)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, customer)
+}
+func (h *Handler) AddUser(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	var customer models.Customer
+	err := c.ShouldBindJSON(&customer)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	customer, err = h.db.AddUser(customer)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, customer)
+}
+func (h *Handler) SignOut(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	p := c.Param("id")
+	id, err := strconv.Atoi(p)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	err = h.db.SignOutUserById(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+}
+func (h *Handler) GetOrders(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	p := c.Param("id")
+	id, err := strconv.Atoi(p)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	orders, err := h.db.GetCustomerOrdersByID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, orders)
 }
